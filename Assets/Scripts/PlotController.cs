894f6e215478c88a66f1fcdc594db0b0f005fe37
Hakej <michaluszak97@gmail.com>
2020-11-07 14:48:40 +0100

    Add prefab brush

---
+++ Assets/Scripts/PlotController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotController : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Hello");
+    }
+}
